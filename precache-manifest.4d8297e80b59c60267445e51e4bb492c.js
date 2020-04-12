74f2f64439b92bfcefae3bf36a589994d978ea14
Saljo <saljo@dexlock.com>
2020-04-12 12:32:48 +0530

    Updates

---
+++ precache-manifest.4d8297e80b59c60267445e51e4bb492c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801b0b53ca999f06525c7aa912ce7341",
+    "url": "/speed-typing/index.html"
+  },
+  {
+    "revision": "6ee4af0823eda79b210b",
+    "url": "/speed-typing/static/css/main.638757c9.chunk.css"
+  },
+  {
+    "revision": "6f7424811979abe61748",
+    "url": "/speed-typing/static/js/2.3c37da41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing/static/js/2.3c37da41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee4af0823eda79b210b",
+    "url": "/speed-typing/static/js/main.8a0f2abd.chunk.js"
+  },
+  {
+    "revision": "869447f48979b8b2b9a7",
+    "url": "/speed-typing/static/js/runtime-main.a1f7b220.js"
+  },
+  {
+    "revision": "1f766ecaae4d48e355a557b455ea5049",
+    "url": "/speed-typing/static/media/speed.1f766eca.svg"
+  },
+  {
+    "revision": "a17d45682a3848283a0750d2a05e8a35",
+    "url": "/speed-typing/static/media/target.a17d4568.svg"
+  }
+]);
